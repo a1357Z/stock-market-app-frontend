1b1b24b8859e682a619b693718317182ac89af3d
ajay yadav <2016meb1145@iitrpr.ac.in>
2021-05-07 22:17:26 +0530

    shifted the data fetching from the api to the app component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@
 //RL8262AMSHINMOF0
 
 import './App.css';
-import React from "react";
+import {React} from "react";
 import {
   BrowserRouter as Router,
   Switch,
@@ -13,8 +13,92 @@ import {
 } from "react-router-dom";
 import Home from './home/Home'
 import View from './view/View'
+import { useEffect,useState,useRef } from 'react';
 
 export default function App() {
+
+  const [dataList, setData] = useState([])
+  const database = useRef([]);
+
+  useEffect(()=> {
+
+    if(dataList.length !== 0){
+        return
+    }
+
+
+    //list of words for which the query is to be performed
+    let charsList=['amazon','microsoft','googl','facebook']
+    let dataArray = []
+    
+    charsList.forEach((char,i)=>{
+
+        fetch(`https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${char}&apikey=RL8262AMSHINMOF0`)
+        .then(res=>res.json())
+        .then(data=>{
+            if(!data.bestMatches){
+                return
+            }
+            console.log('data.bestMatches are',data.bestMatches);
+            data.bestMatches.forEach((data,j)=>{
+
+                let obj={
+                    name : data['2. name'],
+                    symbol : data['1. symbol']
+                }
+
+                fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${obj.symbol}&apikey=RL8262AMSHINMOF0`)
+                .then(res=>res.json())
+                .then(det=>{
+
+                    if(det['Global Quote']){
+                        obj.price = det['Global Quote']['05. price']
+                        obj.volume = det['Global Quote']['06. volume']
+                    }else{
+                        obj.price = 'N.A.'
+                        obj.volume = 'N.A.'
+                    }
+
+                    fetch(`https://www.alphavantage.co/query?function=OVERVIEW&symbol=${obj.symbol}&apikey=RL8262AMSHINMOF0`)
+                    .then(res=>res.json())
+                    .then(dat=>{
+                        //console.log('market cap is ',data);
+                        if(dat.MarketCapitalization){
+                            obj.marketCap = dat.MarketCapitalization
+                        }else{
+                            obj.marketCap = 'N.A.'
+                        }
+                        dataArray.push(obj)
+                        
+                        //console.log('obj is',obj);
+                        if(i===charsList.length-1){
+                            console.log('dataArray is ',dataArray);
+                            database.current = dataArray
+                            //maxIndex.current = dataArray.length - 1
+                            setData(prev=>{
+                                return dataArray
+                            })  
+                        }
+                    })
+                                           
+                })
+
+                
+            })
+          
+        })
+    })
+    // setTimeout(()=>{
+    //     console.log('dataArray is ',dataArray);
+    //     database.current = dataArray
+    //     maxIndex.current = dataArray.length - 1
+    //     setData(prev=>{
+    //         return dataArray
+    //     })       
+    // },4000) 
+})
+
+
   return (
     <Router>
       
@@ -39,7 +123,9 @@ export default function App() {
             <View />
           </Route>
           <Route path="/home">
-            <Home />
+            <Home 
+            data={dataList}
+            />
           </Route>
         </Switch>
       </div>
--- src/home/Home.js
+++ src/home/Home.js
@@ -14,84 +14,98 @@ import Table from '../components/Table'
 import SearchBar from '../components/SearchBar'
 import Pagination from '../components/Pagination'
 
-export default function Home(){
+export default function Home(props){
     
-     const [dataList, setData] = useState([])
+     const [dataList, setData] = useState(props.data)
      const [startIndex,setStartIndex] = useState(0)
      const [endIndex,setEndIndex] = useState(4)
-     const database = useRef([]);
+     const database = useRef(props.data);
+     database.current=props.data
      const maxIndex = useRef(null)
+     maxIndex.current=props.data.length-1
      const minIndex = useRef(0)
 
      const fuse = new Fuse(dataList, {
         keys: ["name", "symbol"],
       });
 
-    useEffect(()=> {
+    // useEffect(()=> {
 
-        if(dataList.length !== 0){
-            return
-        }
+    //     if(dataList.length !== 0){
+    //         return
+    //     }
 
 
-        //list of words for which the query is to be performed
-        let charsList=['amazon','microsoft','googl','facebook']
-        let dataArray = []
+    //     //list of words for which the query is to be performed
+    //     let charsList=['amazon','microsoft','googl','facebook']
+    //     let dataArray = []
         
-        charsList.forEach(char=>{
+    //     charsList.forEach((char,i)=>{
 
-            fetch(`https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${char}&apikey=RL8262AMSHINMOF0`)
-            .then(res=>res.json())
-            .then(data=>{
-                if(!data.bestMatches){
-                    return
-                }
-                data.bestMatches.forEach(data=>{
+    //         fetch(`https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${char}&apikey=RL8262AMSHINMOF0`)
+    //         .then(res=>res.json())
+    //         .then(data=>{
+    //             if(!data.bestMatches){
+    //                 return
+    //             }
+    //             console.log('data.bestMatches are',data.bestMatches);
+    //             data.bestMatches.forEach((data,j)=>{
 
-                    let obj={
-                        name : data['2. name'],
-                        symbol : data['1. symbol']
-                    }
+    //                 let obj={
+    //                     name : data['2. name'],
+    //                     symbol : data['1. symbol']
+    //                 }
 
-                    fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${obj.symbol}&apikey=RL8262AMSHINMOF0`)
-                    .then(res=>res.json())
-                    .then(data=>{
+    //                 fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${obj.symbol}&apikey=RL8262AMSHINMOF0`)
+    //                 .then(res=>res.json())
+    //                 .then(det=>{
 
-                        if(data['Global Quote']){
-                            obj.price = data['Global Quote']['05. price']
-                            obj.volume = data['Global Quote']['06. volume']
-                        }else{
-                            obj.price = 'N.A.'
-                            obj.volume = 'N.A.'
-                        }
+    //                     if(det['Global Quote']){
+    //                         obj.price = det['Global Quote']['05. price']
+    //                         obj.volume = det['Global Quote']['06. volume']
+    //                     }else{
+    //                         obj.price = 'N.A.'
+    //                         obj.volume = 'N.A.'
+    //                     }
+
+    //                     fetch(`https://www.alphavantage.co/query?function=OVERVIEW&symbol=${obj.symbol}&apikey=RL8262AMSHINMOF0`)
+    //                     .then(res=>res.json())
+    //                     .then(dat=>{
+    //                         //console.log('market cap is ',data);
+    //                         if(dat.MarketCapitalization){
+    //                             obj.marketCap = dat.MarketCapitalization
+    //                         }else{
+    //                             obj.marketCap = 'N.A.'
+    //                         }
+    //                         dataArray.push(obj)
+                            
+    //                         //console.log('obj is',obj);
+    //                         if(i===charsList.length-1){
+    //                             console.log('dataArray is ',dataArray);
+    //                             database.current = dataArray
+    //                             maxIndex.current = dataArray.length - 1
+    //                             setData(prev=>{
+    //                                 return dataArray
+    //                             })  
+    //                         }
+    //                     })
                                                
-                    })
+    //                 })
 
-                    fetch(`https://www.alphavantage.co/query?function=OVERVIEW&symbol=${obj.symbol}&apikey=RL8262AMSHINMOF0`)
-                    .then(res=>res.json())
-                    .then(data=>{
-                        //console.log('market cap is ',data);
-                        if(data.MarketCapitalization){
-                            obj.marketCap = data.MarketCapitalization
-                        }else{
-                            obj.marketCap = 'N.A.'
-                        }
-                        dataArray.push(obj)
-                        //console.log('obj is',obj);
-                    })
-                })
+                    
+    //             })
               
-            })
-        })
-        setTimeout(()=>{
-            console.log('dataArray is ',dataArray);
-            database.current = dataArray
-            maxIndex.current = dataArray.length - 1
-            setData(prev=>{
-                return dataArray
-            })       
-        },4000) 
-    })
+    //         })
+    //     })
+    //     // setTimeout(()=>{
+    //     //     console.log('dataArray is ',dataArray);
+    //     //     database.current = dataArray
+    //     //     maxIndex.current = dataArray.length - 1
+    //     //     setData(prev=>{
+    //     //         return dataArray
+    //     //     })       
+    //     // },4000) 
+    // })
 
     let handleSearchChange=(event)=>{
         //console.log(event.target.value);
